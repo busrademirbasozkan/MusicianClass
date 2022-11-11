567e8784bcfd8848674523e04e136bab135d7283
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-11 15:52:12 +0300

    Musician adlı bir class oluşturuldu ve properties eklendi.

---
+++ MusicianClass/Musicians.swift
@@ -0,0 +1,16 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by Büşra Özkan on 11.11.2022.
+//
+
+import Foundation
+
+class Musicians {
+    
+    // Classın properties leri
+    var name = ""
+    var age = 0
+    var instrument = ""
+}
---
+++ MusicianClass/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Büşra Özkan on 11.11.2022.
+//
+
+import Foundation
+
+let james = Musicians() //Bu james'in Musicians sınıfına ait bir obje olduğu anlamına gelir.
+
+james.age = 50 // . koyduktan sonra çıkan age, name gibi özellikler propertiesdir.
+james.name = "James Hetfield"
+james.instrument = "Guitar"
+
+print(james.name)
+
+
